8df91178fe8609d74244bd47e244bf373af86204
heshteo <hesh.teo@gmail.com>
2025-02-20 19:50:41 +0200

    c++ files

---
+++ c++/work2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;  // Integer variable
+    double price = 19.99;  // Double variable
+    char grade = 'A';  // Character variable
+    string name = "HESHIMA Herbert";  // String variable
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Price: $" << price << endl;
+    cout << "Grade: " << grade << endl;
+    
+    return 0;
+}
